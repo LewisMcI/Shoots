e74ddd2f5393fb74531123c12c6fd6f59578b3eb
broanite <lmcint218@caledonian.ac.uk>
2023-10-05 23:07:35 +0100

    Server Authoritative Player Movement Added

--- In Class/Assets/Scripts/PlayerMovement.cs
+++ In Class/Assets/Scripts/PlayerMovement.cs
@@ -20,31 +20,66 @@ public class PlayerMovement : NetworkBehaviour
             return;
 
         movementAnimator.SetBool("isMoving", false);
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
             movementAnimator.SetBool("isMoving", true);
+            HandleMovementServerRpc(1, this.NetworkObjectId);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
             movementAnimator.SetBool("isMoving", true);
+            HandleMovementServerRpc(2, this.NetworkObjectId);
+
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.position += new Vector3(0f, speed * Time.deltaTime, 0f);
             movementAnimator.SetBool("isJump", true);
+            HandleMovementServerRpc(3, this.NetworkObjectId);
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.position -= new Vector3(0f, speed * Time.deltaTime, 0f);
             movementAnimator.SetBool("isCrouch", true);
+            HandleMovementServerRpc(4, this.NetworkObjectId);
         }
         else
             movementAnimator.SetBool("isCrouch", false);
+    }
+
+    [ServerRpc]
+    void HandleMovementServerRpc(int movementdirection, ulong theIDOftheCharacterThatMoves)
+    {
+        Debug.Log("the player " + theIDOftheCharacterThatMoves + " just moves from position " + NetworkManager.Singleton.ConnectedClients[0].PlayerObject.transform.position);
+        HandleMovementClientRpc(movementdirection);
+    }
+
+    [ClientRpc]
+    void HandleMovementClientRpc(int movementDirection)
+    {
+        switch (movementDirection)
+        {
+            case 1:
+                transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+                break;
+            case 2:
+                transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
+                break;
+            case 3:
+                transform.position += new Vector3(0f, speed * Time.deltaTime, 0f);
+                movementAnimator.SetBool("isJump", true);
+                break;
+            case 4:
+                transform.position -= new Vector3(0f, speed * Time.deltaTime, 0f);
+                break;
+
+        }
     }
 
     void OnCollisionEnter2D(Collision2D target)
